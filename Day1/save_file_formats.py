1cf5a8ce8faeb8a421fd70adef398089113fbeda
susantabiswas <susanta18@outlook.com>
2017-11-11 17:06:59 +0530

    save an image in different formats

--- Day1/save_file_formats.py
+++ Day1/save_file_formats.py
@@ -6,12 +6,12 @@ import os
 img = cv2.imread("lenna.tif",-1)
 
 #jpeg file
-jpg = cv2.imwrite('lenna_jpg.jpg')
-print( 'file size jpeg:' + os.path.getsize('lenna_jpg.jpg'))
+jpg = cv2.imwrite('lenna_jpg.jpg',img)
+print( 'file size jpeg:' + str(os.path.getsize('lenna_jpg.jpg')/1024**2) + "MB" ) 
 
 #png file
-png = cv2.imwrite('lenna_png.png')
-print( 'file size png:' + os.path.getsize('lenna_png.png'))
+png = cv2.imwrite('lenna_png.png',img)
+print( 'file size png:' + str(os.path.getsize('lenna_png.png')/1024**2) + "MB")
 
 #tif file 
-print('file size tif' + os.path.getsize('lenna.tif'))
+print('file size tif' + str(os.path.getsize('lenna.tif')/1024**2) + "MB")
